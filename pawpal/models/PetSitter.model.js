4c1723e8eae5b996d4dd16868c96688a9c3f1b0a
Daniel Susetio <dsusetio87@gmail.com>
2024-01-04 14:44:44 +0100

    small changes

---
+++ pawpal/models/PetSitter.model.js
@@ -0,0 +1,29 @@
+
+// const mongoose = require('mongoose');
+
+// const { Schema, model } = require("mongoose");
+
+
+// const petSchema = new Schema({
+//     name: String,
+//     animal: { 
+//       type: String, 
+//       enum: ['Dog', 'Cat'] 
+//     },
+//     breed: String,
+//     age: Number,
+//     temperament: { 
+//       type: String, 
+//       enum: ['Playful', 'Energetic', 'Couch Potato'] 
+//     },
+//     healthAndDiet: String,
+//     // photo: String,
+//     comments: [{ 
+//       type: Schema.Types.ObjectId, 
+//       ref: 'Comment' 
+//     }]
+//   });
+  
+//   const Pet = mongoose.model('Pet', petSchema);
+
+//   module.exports = Pet;
